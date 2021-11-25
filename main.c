8587a3d3b305d688d3d5cab8db62d038650eb535
Heraud <heraud@MacBook-Air-de-Heraud.local>
2021-11-25 10:38:32 +0100

    debut menu

--- main.c
+++ main.c
@@ -15,6 +15,24 @@ typedef struct data {
 
 void ouverture(void);
 void menu(void);
+void afficher(void);
+
+void afficher(){
+    FILE *fic;
+    char ligne[81];
+    char *ptr_chaine ;
+    short int num_ligne = 1 ;
+    short int data_entier;
+    double data_reel;
+    char data_chaine[11];
+    ouverture();
+    while ( fgets( ligne, 81, fic) != NULL )
+    {
+        printf("\n Ligne %2hd :", num_ligne );
+        num_ligne++ ;
+        ptr_chaine = strtok (ligne, ";");
+}
+}
 
 void menu(){
     int c=0;
@@ -33,22 +51,35 @@ void menu(){
     switch(c)
        {
            case 1:
+           printf("test");
+            menu();
                break;
            case 2:
+           printf("test");
+           menu();
                break;
            case 3:
+           printf("test");
+           menu();
                break;
            case 4:
+           afficher();
+           menu();
                break;
            case 5:
+           printf("test");
+           menu();
                break;
            case 6:
+           printf("test");
+           menu();
                break;
            case 9:
                break;
        default: printf("error");
        }
 }
+
 void ouverture(){
     FILE *fic;
     fic = fopen("chemin", "a+");
@@ -62,9 +93,8 @@ void ouverture(){
     }
 }
 
-int main () {
+int main(){
     ouverture();
     int option=0;
     return 0;
 }
-    
