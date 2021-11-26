8fc189d2a7f5d66d4bf9d0e02e509ef00322828e
Heraud <heraud260@gmail.com>
2021-11-26 16:45:53 +0100

    rebuild afficher, creer structure

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 #define chemin "annuaire500.csv"
 
-typedef struct data {
+struct data {
     char prenom[30];
     char nom[30];
     char ville[20];
@@ -21,14 +21,52 @@ typedef struct data {
     char numero[10];
     char mail[50];
     char metier[20];
-} DATA ;
+}
+struct data personne[10];
 
 void ouverture(void);
 void menu(void);
 void afficher(void);
-int creerstruct(void);
-void afficherstruct(DATA values[]);
+void creerstruct(void);
+void afficherstruct(void);
 
+void creerstruct(){
+  char temp[MAXCHAR];
+  char r[30];
+  int i =0;
+  char * pointer;
+  FILE *fp;
+  fp = fopen(chemin, "r");
+  if (fp == NULL){
+    printf ("error");
+    exit(1);
+  }
+  while (!feof(fp)){
+    fgets(r, 130, import);
+    pointer = strtok(r,";");
+    while (pointer != NULL){
+      strcpy(temp[i].nom,pointer);
+      pointer = strtok(NULL,";");
+      strcpy(temp[i].prenom,pointer);
+      pointer = strtok(NULL,";");
+      strcpy(temp[i].ville,pointer);
+      pointer = strtok(NULL,";");
+      strcpy(temp[i].cdePostal,pointer);
+      pointer = strtok(NULL,";");
+      strcpy(temp[i].numero,pointer);
+      pointer = strtok(NULL,";");
+      i++;
+    }
+  }
+  fclose(fp);
+  afficherstruct();
+}
+void afficherstruct(){
+  int i=0;
+  for (i=0;i<10;i++){
+    printf("%s_%s_%s",personne[i].nom,personne[i].prenom,personne[i].ville);
+  }
+}
 
 void afficher(){
    FILE *fp = fopen(chemin,"r");
@@ -49,61 +87,8 @@ void afficher(){
         }
         printf("\n");
     }
-
-int creerstruct(){
-    FILE *fp;
-
-    fp = fopen("chemin","r");
-    printf("ouverture ok");
-    if (fp == NULL){
-        printf("error");
-        return 0;
-    }
-    char buff[MAXCHAR];
-    int row = 0;
-    int col = 0;
-    DATA values[999];
-    int i=0;
-    while (fgets(buff,1024,fp)){
-        col = 0;
-        row++;
-        char *field = strtok("buff", ";");
-        while (field){
-            if(col == 0)
-                strcpy(values[i].prenom, field);
-            if(col == 1)
-                strcpy(values[i].nom, field);
-            if(col == 2)
-                strcpy(values[i].ville, field);
-            if(col == 3)
-                strcpy(values[i].cdePostal, field);
-            if(col == 4)
-                strcpy(values[i].numero, field);
-            if(col == 5)
-                strcpy(values[i].mail, field);
-            if(col == 6)
-                strcpy(values[i].metier, field);
-
-            field = strtok("NULL", ";");
-            col++;
-        }
-        i++;
-    }
-    fclose(fp);
-
-
-    afficherstruct(values);
-
-
-    return 0;
 }
 
-
-void afficherstruct(DATA values[]){
-    for (int i=0; i<999; i++){
-        printf("prenom : %s,nom : %s, ville : %s, cdePostal : %s, numero : %s, mail : %s, metier : %s",values[i].prenom,values[i].nom,values[i].ville,values[i].cdePostal,values[i].numero,values[i].mail,values[i].metier);
-    }
-}
 
 void menu(){
     int c=0;
