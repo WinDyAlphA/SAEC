90b98eb7b780aa03909c967e44d93ae8d838bc67
Heraud <heraud260@gmail.com>
2021-11-26 14:57:39 +0100

    bug lecture du ficher fix

--- main.c
+++ main.c
@@ -8,33 +8,102 @@
 #include "utilities.h"
 #include "func.h"
 
-#define MAXCHAR 1000
+#define MAXCHAR 1024
+#define MAX_STR_LEN 256
 
-#define chemin "phone_book.csv"
+#define chemin "annuaire500.csv"
 
 typedef struct data {
-    char nom[20];
+    char prenom[30];
+    char nom[30];
+    char ville[20];
+    char cdePostal[5];
+    char numero[10];
+    char mail[50];
+    char metier[20];
 } DATA ;
 
 void ouverture(void);
 void menu(void);
 void afficher(void);
+int creerstruct(void);
+void afficherstruct(DATA values[]);
+
 
 void afficher(){
-    FILE *fp;
+   FILE *fp = fopen(chemin,"r");
     char row[MAXCHAR];
 
-    fp = fopen("annuaire5000.csv","r");
+    if  (fp == NULL){
+        perror("error");
+        exit(1);
+    }
 
-    ;
+    while (fgets(row, sizeof(row), fp)){
+        char *token;
+        token = strtok(row, ";");
 
-    while (feof(fp) != true)
-    {
-        fgets(row, MAXCHAR, fp);
-        printf("Row: %s", row);
+        while (token != NULL){
+            printf("%s", token);
+            token = strtok(NULL, ";");
+        }
+        printf("\n");
+    }
+    
+}
+int creerstruct(){
+    FILE *fp;
+
+    fp = fopen("chemin","r");
+    printf("ouverture ok");
+    if (fp == NULL){
+        printf("error");
+        return 0;
+    }
+    char buff[MAXCHAR];
+    int row = 0;
+    int col = 0;
+    DATA values[999];
+    int i=0;
+    while (fgets(buff,1024,fp)){
+        col = 0;
+        row++;
+        char *field = strtok("buff", ";");
+        while (field){
+            if(col == 0)
+                strcpy(values[i].prenom, field);
+            if(col == 1)
+                strcpy(values[i].nom, field);
+            if(col == 2)
+                strcpy(values[i].ville, field);
+            if(col == 3)
+                strcpy(values[i].cdePostal, field);
+            if(col == 4)
+                strcpy(values[i].numero, field);
+            if(col == 5)
+                strcpy(values[i].mail, field);
+            if(col == 6)
+                strcpy(values[i].metier, field);
+
+            field = strtok("NULL", ";");
+            col++;
+        }
+        i++;
+    }
+    fclose(fp);
+
+
+    afficherstruct(values);
+
+
+    return 0;
+}
+
+void afficherstruct(DATA values[]){
+    for (int i=0; i<999; i++){
+        printf("prenom : %s,nom : %s, ville : %s, cdePostal : %s, numero : %s, mail : %s, metier : %s",values[i].prenom,values[i].nom,values[i].ville,values[i].cdePostal,values[i].numero,values[i].mail,values[i].metier);
     }
 }
-    
 
 
 
